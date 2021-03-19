22f84051dcf0bd369c5e9322c8bf84cf3f24433d
王治杨 <wangzhiyang06@countrygarden.com.cn>
2021-03-19 14:32:00 +0800

    Init

---
+++ src/main/java/com/youngwang/webflux/controller/SimpleController.java
@@ -0,0 +1,88 @@
+package com.youngwang.webflux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/simple")
+public class SimpleController {
+
+
+
+    // 注意，在webflux 中，接口只需要定义 Mono 或 Flux，无需订阅
+
+    @GetMapping("/mono")
+    public Mono<String> simpleMono(@RequestParam("name") String name) {
+        // 简单示例，直接包装值然后返回
+        return Mono.just("hello " + name);
+    }
+
+    @GetMapping("/mono/order/just")
+    public Mono<String> simpleMonoJustOrder(@RequestParam("name") String name) {
+        // 直接包装值,注意打印顺序
+        // 和 defer 比较，这里是直接执行
+        Mono<String> result = Mono.just(getResponse(name));
+        System.out.println("已获取到 Mono<String> result ");
+        return result;
+    }
+
+    @GetMapping("/mono/order/defer")
+    public Mono<String> simpleMonoDeferOrder(@RequestParam("name") String name) {
+        // 直接包装值,注意打印顺序
+        // 和 just 比较，这里是延迟执行
+        Mono<String> result = Mono.defer(() -> Mono.just(getResponse(name)));
+        System.out.println("已获取到 Mono<defer> result ");
+        return result;
+    }
+
+    private String getResponse(String name) {
+        String response = "hello " + name;
+        System.out.println(MessageFormat.format("调用 getResponse({0}) 返回值为 \"{1}\"", name, response));
+        return response;
+    }
+
+
+    @GetMapping("/flux/just")
+    public Flux<String> simpleFluxJust(@RequestParam("name") String name) {
+        // 直接包装值
+        // ！！！
+        // 特别注意： 返回 Flux<String> ，会被合并为一个 String，而不是 String 数组
+        // @see https://stackoverflow.com/questions/48421597/returning-fluxstring-from-spring-webflux-returns-one-string-instead-of-array-o
+        // ！！！
+        return Flux.just("hello ", name);
+    }
+
+    @GetMapping("/flux/just/String/array")
+    public Mono<List<String>> simpleFluxJustStringArray(@RequestParam("name") String name) {
+        // 直接包装值
+        // ！！！
+        // 特别注意： 返回 Flux<String> ，会被合并为一个 String，而不是 String 数组
+        // @see https://stackoverflow.com/questions/48421597/returning-fluxstring-from-spring-webflux-returns-one-string-instead-of-array-o
+        // ！！！
+        return Flux.just("hello ", name).collectList();
+    }
+
+    @GetMapping("/flux/from")
+    public Flux<Integer> simpleFluxFrom(@RequestParam("name") String name) {
+        // 包装Stream返回
+        // 类似的还有 Flux.fromArray , Flux.fromIterable
+        return Flux.fromStream(
+                Stream.of(name.split(""))
+        ).map(s -> (int) s.charAt(0));
+    }
+
+    @GetMapping("/flux/range")
+    public Flux<Integer> simpleFluxRange(@RequestParam("size") Integer size) {
+        // range 会从 start 累加指定的次数，形成的元素放入流中
+        return Flux.range(0, size);
+    }
+
+}
---
+++ src/test/java/com/youngwang/wenflux/test/SimpleControllerTest.java
@@ -0,0 +1,102 @@
+package com.youngwang.wenflux.test;
+
+import com.youngwang.webflux.WebfluxApplication;
+import com.youngwang.webflux.controller.SimpleController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = WebfluxApplication.class)
+@WebFluxTest(controllers = SimpleController.class)
+public class SimpleControllerTest {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void testSimpleMono() {
+        webClient.get()
+                .uri("/simple/mono?name={name}", Map.of("name", "张三"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("hello 张三")
+                .value(res -> System.out.println("testSimpleMono: " + res));
+    }
+
+    @Test
+    public void testSimpleMonoJustOrder() {
+        webClient.get()
+                .uri("/simple/mono/order/just?name={name}", Map.of("name", "张三"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("hello 张三")
+                .value(res -> System.out.println("testSimpleMonoJustOrder: " + res));
+    }
+
+    @Test
+    public void testSimpleMonoDeferOrder() {
+        webClient.get()
+                .uri("/simple/mono/order/defer?name={name}", Map.of("name", "张三"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("hello 张三")
+                .value(res -> System.out.println("testSimpleMonoDeferOrder: " + res));
+    }
+
+    @Test
+    public void testSimpleFluxJust() {
+        webClient.get()
+                .uri("/simple/flux/just?name={name}", Map.of("name", "张三"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("hello 张三")
+                .value(v -> System.out.println("testSimpleFluxJust: " + v));
+    }
+
+    @Test
+    public void testSimpleFluxJustStringArray() {
+        webClient.get()
+                .uri("/simple/flux/just/String/array?name={name}", Map.of("name", "张三"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBody(String.class)
+                .value(v -> System.out.println("testSimpleFluxJustStringArray: " + v));
+    }
+
+    @Test
+    public void testSimpleFluxFrom() {
+        webClient.get()
+                .uri("/simple/flux/from?name={name}", Map.of("name", "zhangsan"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBodyList(Integer.class)
+                .hasSize(8)
+                .value(list -> System.out.println("testSimpleFluxFrom: " + list.stream().map(String::valueOf).collect(Collectors.joining(","))));
+    }
+
+    @Test
+    public void testSimpleFluxRange() {
+        webClient.get()
+                .uri("/simple/flux/range?size={size}", Map.of("size", "3"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBodyList(Integer.class)
+                .hasSize(3)
+                .contains(0, 1, 2)
+                .value(l -> System.out.println("testSimpleFluxRange: " + l.stream().map(String::valueOf).collect(Collectors.joining(","))));
+    }
+
+}
